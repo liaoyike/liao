5809a0ee6e0a0f95383aebecc8ffbef521872743
liaoyk <liaoyk0@163.com>
2019-11-06 15:30:21 +0800

    分支member

---
+++ Git_demo/src/main/java/com/itheima/domain/Member.java
@@ -0,0 +1,13 @@
+package com.itheima.domain;
+
+/**
+ * @Description:
+ * @Author: liaoyike
+ * @Created Date: 2019/11/6 15:29
+ * @LastModifyDate:
+ * @LastModifyBy:
+ * @Version:
+ */
+public class Member {
+    
+}
