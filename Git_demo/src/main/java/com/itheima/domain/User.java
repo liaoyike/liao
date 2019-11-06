f19abbbc78bd60c717296aeeefe95678967a839d
liaoyk <liaoyk0@163.com>
2019-11-06 11:35:42 +0800

    123

---
+++ Git_demo/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,13 @@
+package com.itheima.domain;
+
+/**
+ * @Description:
+ * @Author: liaoyike
+ * @Created Date: 2019/11/6 11:32
+ * @LastModifyDate:
+ * @LastModifyBy:
+ * @Version:
+ */
+public class User {
+    private Integer id;
+}
